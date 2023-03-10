80ba4219b995246c3f143e9164b79075491cae7c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-03-10 06:50:10 +0300

    Added seed data for courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+courses = Course.create([
+    {name: "English", cover_photo: "https://images.unsplash.com/photo-1556740732-8f9a9d4d4d6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW5nbGlzaCUyMGZvcm1hdCUyMGNvdXJzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", start_date: "2021-03-01", level: "Beginner", lessons: 10},
+
+    {name: "Math", cover_photo: "https://images.unsplash.com/photo-1556740732-8f9a9d4d4d6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW5nbGlzaCUyMGZvcm1hdCUyMGNvdXJzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", start_date: "2021-03-01", level: "Beginner", lessons: 10},
+
+
+    {name: "Science", cover_photo: "https://images.unsplash.com/photo-1556740732-8f9a9d4d4d6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW5nbGlzaCUyMGZvcm1hdCUyMGNvdXJzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", start_date: "2021-03-01", level: "Beginner", lessons: 10},
+
+    {name: "History", cover_photo: "https://images.unsplash.com/photo-1556740732-8f9a9d4d4d6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW5nbGlzaCUyMGZvcm1hdCUyMGNvdXJzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", start_date: "2021-03-01", level: "Beginner", lessons: 10},
+
+    {name: "Art", cover_photo: "https://images.unsplash.com/photo-1556740732-8f9a9d4d4d6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW5nbGlzaCUyMGZvcm1hdCUyMGNvdXJzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", start_date: "2021-03-01", level: "Beginner", lessons: 10},
+
+    {name: "Music", cover_photo: "https://images.unsplash.com/photo-1556740732-8f9a9d4d4d6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW5nbGlzaCUyMGZvcm1hdCUyMGNvdXJzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", start_date: "2021-03-01", level: "Beginner", lessons: 10},             
+
+
+
+    {name: "English", cover_photo: "https://images.unsplash.com/photo-1556740732-8f9a9d4d4d6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW5nbGlzaCUyMGZvcm1hdCUyMGNvdXJzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", start_date: "2021-03-01", level: "Intermediate", lessons: 10},
+
+
+]   )
